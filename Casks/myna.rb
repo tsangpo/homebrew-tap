7591696536907ce2a9cb5563d093572f8061d24d
Canpeng Zheng <canpeng.zheng@compass.com>
2026-03-30 23:20:28 -0400

    Update myna cask to 0.3.0

--- Casks/myna.rb
+++ Casks/myna.rb
@@ -1,8 +1,8 @@
 cask "myna" do
-  version "0.2.0"
-  sha256 "b1bb3b83fc21ec0831fa57d599075b16dba27ec94ff8ecf2e7ee91a8ab8187db"
+  version "0.3.0"
+  sha256 "e8a30dc1b2e2b4a29de1318f3cbdd0ca23e71413c1126a24b8859b2b187fdbe6"
 
-  url "https://github.com/tsangpo/homebrew-tap/releases/download/myna-v0.2.0/Myna.dmg",
+  url "https://github.com/tsangpo/homebrew-tap/releases/download/myna-v0.3.0/Myna.dmg",
       verified: "github.com/tsangpo/homebrew-tap/"
   name "Myna"
   desc "AI assistant toolkit for macOS with text analysis, dictation, and screen capture"
